40ec08268eddb196cb93e520dfa45332e97e13c9
ASaidOguz <ahmetsaid2136@gmail.com>
2022-06-12 01:47:42 +0300

    iitial commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var path = require('path');
 var express = require('express');
 
-var app = express();
+var myApp = express();
 
-app.use(express.static(path.join(__dirname, 'Discordbot')));
-app.set('port', process.env.PORT || 8080);
+myApp.use(express.static(path.join(__dirname, 'Movie API')));
+myApp.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
